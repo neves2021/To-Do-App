7c253a70fe668f0cedde44ec7c5360f422836795
joão <joao.correia.neves@outlook.pt>
2024-03-16 14:59:27 +0000

    modified:   to-do/src/App.jsx new file:   to-do/src/components/Filter.jsx new file:   to-do/src/components/Search.jsx modified:   to-do/src/components/Todo.jsx

---
+++ to-do/src/components/Search.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Search = ({ search, setSearch }) => {
+  return (
+    <div className="search">
+      <h2>Search:</h2>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      ></input>
+    </div>
+  );
+};
+
+export default Search;
